f059008e7f2832f64608c652324589544106aa3a
MedAmineChakroun <164560880+MedAmineChakroun@users.noreply.github.com>
2024-04-29 11:17:41 +0100

    adding quiz game to fedi branch

---
+++ projet d'integration/Backend (server-Side)/QuizzGame/src/main/java/com/game/quiz/QuizzGameApplication.java
@@ -0,0 +1,14 @@
+package com.game.quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizzGameApplication.class, args);
+		System.out.println("hello team");
+	}
+
+}
